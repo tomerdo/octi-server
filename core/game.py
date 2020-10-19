4064839418b90eb0802d95733e5dd4da21b4454b
Tomer Dobkin <tomer.dobkin@gmai.com>
2020-10-19 04:49:31 +0300

    adding to state

--- core/game.py
+++ core/game.py
@@ -8,12 +8,18 @@ class Game:
         self.blue_player = blue_player
         self.round_num = 0
         self.action_num = 0
-        self.blue_state = create_state(blue_player)
-        self.red_state = create_state(red_player)
+        self.blue_state = create_state(blue_player, 'BLUE')
+        self.red_state = create_state(red_player, 'RED')
+        self.current_turn = 'BLUE'
 
     def to_response(self):
-        return {'status': 'active', 'red_player_state': self.red_state, 'blue_player_state': self.blue_state, \
-                'round_num': self.round_num, 'action_num': self.action_num}
+        return {'status': 'active',
+                'red_player_state': self.red_state,
+                'blue_player_state': self.blue_state,
+                'round_num': self.round_num,
+                'action_num': self.action_num,
+                'current_turn': self.current_turn
+                }
 
 
 def choose_red_and_blue_randomly(first_player, second_player):
@@ -30,5 +36,18 @@ def choose_red_and_blue_randomly(first_player, second_player):
     return red_player, blue_player
 
 
-def create_state(player):
+def create_pieces(color):
+    """
+    TODO implement this, take from the client
+    :param color: player color (base) - the location is according to base
+    :return:
+    """
     return []
+
+
+def create_state(player, color):
+    return {
+        'color': color,
+        'pieces': create_pieces(color),
+        'ammunition': 10
+    }
